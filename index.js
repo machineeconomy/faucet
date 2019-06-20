2f86e497602aa4bb8be9e823c2e26b10f19cd766
huhn <sehe89@gmail.com>
2019-06-20 18:49:40 +0200

    add express app and endpoints

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+var app = require('express')();
+var cors = require('cors')
+app.use(cors())
+
+
 var core = require('@iota/core');
 var iota = core.composeAPI({
     provider: 'https://nodes.devnet.thetangle.org:443'
 })
 
-const seed = ''
+const dotenv = require('dotenv');
+dotenv.config();
 
-var payoutaddress = ''
+const PORT = process.env.PORT
+const SEED = process.env.SEED
+
 var keyIndex = 0
 var payoutamount = 10
 //before sending you should check if the previous transaction is confirmed and if it's unconfirmed promote/reattach it or wait
 //sendIotas(payoutaddress, keyIndex)
+
+app.get('/', (req, res) => res.send(`Welcome to the AKITA IOTA Devnet Faucet`))
+
+app.post('/send_tokens', function (request, response) {
+    if (request.query.hasOwnProperty('address')) {
+        let payoutaddress = request.query.address
+        response.json({  status: "ok", address: payoutaddress, message: "Sent IOTA Tokens to the given address." });
+    } else {
+        response.json({ status: "error", message: "Address not in the query." });
+    }
+})
+
+var server = app.listen(PORT, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
 
 async function sendIotas(payoutaddress, keyIndex) {
     let inputAddress = core.generateAddress(seed, keyIndex, 2)
@@ -39,7 +65,7 @@ async function sendIotas(payoutaddress, keyIndex) {
         }]
     }
     iota
-        .prepareTransfers(seed, transfers, options)
+        .prepareTransfers(SEED, transfers, options)
         .then(trytes => iota.sendTrytes(trytes, 3, 9))
         .then(bundle => {
             console.log('Transfer sent: https://devnet.thetangle.org/transaction/' + bundle[0].hash)
